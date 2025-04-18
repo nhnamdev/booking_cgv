288978f61e4bbe13ea94b0d0077c3106339bd72d
Nam <22130173@st.hcmuaf.edu.vn>
2025-04-18 13:56:59 +0700

    push brand

--- database/movie_ticket_booking_website.sql
+++ database/movie_ticket_booking_website.sql
@@ -1,31 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 28, 2023 at 10:41 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
+CREATE DATABASE movie_ticket_booking_website
+
 -- Database: `movie_ticket_booking_website`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `features`
---
+-
 
 CREATE TABLE `features` (
   `id` int(11) NOT NULL,
